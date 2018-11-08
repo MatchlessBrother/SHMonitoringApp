467724e6458e6ad4f4e1ca5e7fc93cf6ab84bbbd
yxl <1172246321@qq.com>
2018-11-08 19:21:43 +0800

    ...

--- app/src/main/java/company/petrifaction/client/ui/main/activity/view/SignInAct.java
+++ app/src/main/java/company/petrifaction/client/ui/main/activity/view/SignInAct.java
@@ -6,9 +6,13 @@ import android.widget.Button;
 import android.content.Intent;
 import android.widget.EditText;
 import android.widget.TextView;
+import com.gyf.barlibrary.BarHide;
+import android.view.WindowManager;
 import company.petrifaction.client.R;
 import android.content.pm.PackageInfo;
+import com.gyf.barlibrary.ImmersionBar;
 import android.content.pm.PackageManager;
+import com.gyf.barlibrary.OnKeyboardListener;
 import company.petrifaction.client.base.BaseAct;
 import com.yuan.devlibrary._12_______Utils.SharepreferenceUtils;
 import company.petrifaction.client.ui.main.activity.view_v.SignInAct_V;
@@ -21,6 +25,7 @@ public class SignInAct extends BaseAct implements SignInAct_V,View.OnClickListen
     private EditText mSigninAccount;
     private EditText mSigninPassword;
     private SignInPresenter mSignInPresenter;
+    private static final String LOG_TAG = SignInAct.class.getSimpleName();
 
     protected int setLayoutResID()
     {
@@ -31,6 +36,19 @@ public class SignInAct extends BaseAct implements SignInAct_V,View.OnClickListen
     protected void initWidgets(View rootView)
     {
         super.initWidgets(rootView);
+        ImmersionBar.with(this).titleBar(R.id.signin_all).navigationBarColor(R.color.colorPrimary).navigationBarAlpha(0f)
+                .hideBar(BarHide.FLAG_SHOW_BAR).navigationBarEnable(true).navigationBarWithKitkatEnable(true)
+                .statusBarDarkFont(false).flymeOSStatusBarFontColor(R.color.white).fullScreen(false).keyboardEnable(true)
+                .keyboardMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE).setOnKeyboardListener(new OnKeyboardListener()
+        {
+            public void onKeyboardChange(boolean status,int keyboardHeight)
+            {
+                if(status)
+                    Log.w(LOG_TAG, "SoftKeyBoard：Turn On！");
+                else
+                    Log.w(LOG_TAG, "SoftKeyBoard：Turn On！");
+            }
+        }).init();
         mSigninLogin = (Button)rootView.findViewById(R.id.signin_login);
         mSigninVersion = (TextView)rootView.findViewById(R.id.signin_version);
         mSigninAccount = (EditText)rootView.findViewById(R.id.signin_account);
